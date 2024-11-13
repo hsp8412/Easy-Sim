f466a462dcd33e9f3359b8116a59104305e3b762
Henry Pham <115191366+21henryp@users.noreply.github.com>
2024-11-12 19:26:21 -0700

    updated Card component

--- frontend/src/components/customer/card.tsx
+++ frontend/src/components/customer/card.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import React, { FC, ReactNode } from "react";
 
 interface CardProps {
   header?: string | null;
@@ -7,7 +7,7 @@ interface CardProps {
 
 interface cardDiv {
   title?: string | null;
-  content: ReactNode;
+  content?: ReactNode[];
   buttonText?: string | null;
   onClick?: () => void;
 }
@@ -16,13 +16,17 @@ const AddDiv: FC<cardDiv> = ({ title, content, buttonText, onClick }) => {
   return (
     <div className="bg-gray-100 p-4">
       <h5 className="text-lg font-bold mb-2">{title}</h5>
-      <p className="mb-4">{content}</p>
-      <button
-        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        onClick={onClick}
-      >
-        {buttonText}
-      </button>
+      <div className="mb-4">
+        {content && content.map((node, index) => <div key={index}>{node}</div>)}
+      </div>
+      {buttonText && (
+        <button
+          className="bg-blue-400 text-white py-1 px-4 rounded-full hover:bg-blue-600"
+          onClick={onClick}
+        >
+          {buttonText}
+        </button>
+      )}
     </div>
   );
 };
@@ -31,7 +35,9 @@ const Card: FC<CardProps> = ({ header, cdivs }) => (
   <div className="w-full sm:w-1/2 md:w-1/3 p-4 text-black">
     <div className="border border-gray-300 rounded-lg shadow-lg grid divide-y divide-gray-400">
       {header ? (
-        <div className="bg-gray-100 p-4 font-semibold">{header}</div>
+        <div className="bg-gray-100 p-4 font-semibold rounded-t-lg">
+          {header}
+        </div>
       ) : null}
       {cdivs && cdivs.map((cdiv, index) => <AddDiv key={index} {...cdiv} />)}
     </div>
--- frontend/src/components/customer/profile.tsx
+++ frontend/src/components/customer/profile.tsx
@@ -20,32 +20,84 @@ const Profile = () => {
             header={`Hi, ${user?.firstName} ${user?.lastName}`}
             cdivs={[
               {
-                content:
-                  "With supporting text below as a natural lead-in to additional content.",
-                buttonText: "Go somewhere",
+                content: [
+                  <h6 className="font-semibold text-sm text-left">
+                    Current Email Address
+                  </h6>,
+                  <input
+                    className="border rounded-lg border-gray-700"
+                    style={{ width: "100%" }}
+                  />,
+                  <h6 className="font-semibold text-sm text-left pt-3">
+                    Update Email Address
+                  </h6>,
+                  <input
+                    className="border rounded-lg border-gray-700"
+                    style={{ width: "100%" }}
+                  />,
+                ],
+                buttonText: "Update Email",
+                onClick: () => {
+                  alert("hi");
+                },
               },
               {
-                content:
-                  "With supporting text below as a natural lead-in to additional content.",
-                buttonText: "Go somewhere",
+                content: [
+                  <h6 className="font-semibold text-sm text-left">
+                    Current Password
+                  </h6>,
+                  <input
+                    className="border rounded-lg border-gray-700"
+                    style={{ width: "100%" }}
+                  />,
+                  <h6 className="font-semibold text-sm text-left pt-3">
+                    New Password
+                  </h6>,
+                  <input
+                    className="border rounded-lg border-gray-700"
+                    style={{ width: "100%" }}
+                  />,
+                  <h6 className="font-semibold text-sm text-left pt-3">
+                    Confirm Password
+                  </h6>,
+                  <input
+                    className="border rounded-lg border-gray-700"
+                    style={{ width: "100%" }}
+                  />,
+                ],
+                buttonText: "Update Password",
+                onClick: () => {
+                  alert("hi");
+                },
+              },
+              {
+                content: [
+                  <button className="bg-transparent text-red-600 font-semibold py-1 px-4 rounded-full hover:bg-red-600 hover:text-white">
+                    Delete Account
+                  </button>,
+                ],
               },
             ]}
           />
           <Card
+            header="Current Data Plan"
             cdivs={[
               {
                 title: "Another special title",
-                content: "Additional text here for another card.",
-                buttonText: "Learn more",
+                content: ["Current plan here."],
+                buttonText: "Request for Refund",
+              },
+              {
+                content: ["Data statistics"],
               },
             ]}
           />
           <Card
+            header="Previous Purchases"
             cdivs={[
               {
                 title: "More treatments",
-                content: "This card has different supporting text.",
-                buttonText: "Discover",
+                content: ["To have previous purchase plans."],
               },
             ]}
           />
