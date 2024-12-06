b2d23b29f5fd2fe5c670bb01d5580f9067e4a798
Joshua Tolentino <j9256t@gmail.com>
2024-12-05 18:34:59 -0700

    proposal admin functionality

--- backend/routes/proposal.js
+++ backend/routes/proposal.js
@@ -1,14 +1,19 @@
 import {carrier_auth} from "../middleware/carrier_auth.js";
+import {admin_auth} from "../middleware/admin_auth.js";
 import {
   getMyProposals,
   createNewProposal,
+  getAllProposals
 } from "../controllers/proposalsController.js";
 import express from "express";
 
 const router = express.Router();
 
+// Carrier routes
 router.get("/get-my-proposals", carrier_auth, getMyProposals);
-
 router.post("/", carrier_auth, createNewProposal);
+
+// Admin routes
+router.get("/all", admin_auth, getAllProposals);
 
 export default router;
